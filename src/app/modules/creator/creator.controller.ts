47d5222301f09dce9c956f45a0ac782b3e84b300
saurav11sarkar <sarkarsaurav11@outlook.com>
2025-09-18 17:18:15 +0600

    creator get all single creator update creator delete

--- src/app/modules/creator/creator.controller.ts
+++ src/app/modules/creator/creator.controller.ts
@@ -1,3 +1,4 @@
+import pick from '../../helper/pick';
 import catchAsync from '../../utils/catchAsycn';
 import sendResponse from '../../utils/sendResponse';
 import { creatorService } from './creator.service';
@@ -5,7 +6,7 @@ import { creatorService } from './creator.service';
 const requestCreator = catchAsync(async (req, res) => {
   const files = req.files as Express.Multer.File[];
   const fromData = req.body.data ? JSON.parse(req.body.data) : req.body;
-  
+
   const result = await creatorService.requestCreator(fromData, files);
 
   sendResponse(res, {
@@ -16,6 +17,86 @@ const requestCreator = catchAsync(async (req, res) => {
   });
 });
 
+const getAllCreators = catchAsync(async (req, res) => {
+  const filters = pick(req.query, [
+    'searchTerm',
+    'fullName',
+    'phoneNumber',
+    'email',
+    'bio',
+    'description',
+    'status',
+    'interests',
+  ]);
+  const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
+  const result = await creatorService.getAllCreators(filters, options);
+
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: 'Creators retrieved successfully',
+    data: result,
+  });
+});
+
+const singleCreator = catchAsync(async (req, res) => {
+  const result = await creatorService.singleCreator(req.params.id);
+
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: 'Creator retrieved successfully',
+    data: result,
+  });
+});
+
+const updatedCreator = catchAsync(async (req, res) => {
+  const files = req.files as Express.Multer.File[];
+  const fromData = req.body.data ? JSON.parse(req.body.data) : req.body;
+  const result = await creatorService.updatedCreator(
+    req.params.id,
+    fromData,
+    files,
+  );
+
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: 'Creator updated successfully',
+    data: result,
+  });
+});
+
+const deleteCreator = catchAsync(async (req, res) => {
+  const result = await creatorService.deleteCreator(req.params.id);
+
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: 'Creator deleted successfully',
+    data: result,
+  });
+});
+
+const updatedStatus = catchAsync(async (req, res) => {
+  const { id } = req.params;
+  const { status } = req.body;
+
+  const result = await creatorService.updatedStatus(id, status);
+
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: 'Agent status updated successfully',
+    data: result,
+  });
+});
+
 export const creatorController = {
   requestCreator,
+  getAllCreators,
+  singleCreator,
+  updatedCreator,
+  deleteCreator,
+  updatedStatus
 };
--- src/app/modules/creator/creator.service.ts
+++ src/app/modules/creator/creator.service.ts
@@ -1,5 +1,6 @@
 import AppError from '../../error/appError';
 import { fileUploader } from '../../helper/fileUploder';
+import pagination, { IOption } from '../../helper/pagenation';
 import { ICreator } from './creator.interface';
 import Creator from './creator.model';
 
@@ -29,6 +30,104 @@ const requestCreator = async (
   return result;
 };
 
+const getAllCreators = async (params: any, options: IOption) => {
+  const { page, limit, skip, sortBy, sortOrder } = pagination(options);
+
+  const { searchTerm, ...filterData } = params;
+
+  const searchableFields = [
+    'fullName',
+    'phoneNumber',
+    'email',
+    'bio',
+    'description',
+    'status',
+    'interests',
+  ];
+
+  const andCondition: any[] = [];
+
+  // search filter
+  if (searchTerm) {
+    andCondition.push({
+      $or: searchableFields.map((field) => ({
+        [field]: { $regex: searchTerm, $options: 'i' },
+      })),
+    });
+  }
+
+  // exact field filter
+  if (Object.keys(filterData).length > 0) {
+    andCondition.push({
+      $and: Object.entries(filterData).map(([field, value]) => ({
+        [field]: value,
+      })),
+    });
+  }
+
+  const whereConditions = andCondition.length > 0 ? { $and: andCondition } : {};
+
+  const result = await Creator.find(whereConditions)
+    .skip(skip)
+    .limit(limit)
+    .sort({ [sortBy]: sortOrder } as any);
+
+  const total = await Creator.countDocuments(whereConditions);
+
+  return {
+    meta: {
+      page,
+      limit,
+      total,
+    },
+    data: result,
+  };
+};
+
+const singleCreator = async (id: string) => {
+  const result = await Creator.findById(id);
+  if (!result) throw new AppError(404, 'Creator not found');
+  return result;
+};
+
+const updatedCreator = async (
+  id: string,
+  payload: Partial<ICreator>,
+  files?: Express.Multer.File[],
+) => {
+  if (files && files.length > 0) {
+    const uploadedImages = await Promise.all(
+      files.map(async (file) => {
+        const res = await fileUploader.uploadToCloudinary(file);
+        return res.secure_url;
+      }),
+    );
+    payload.image = uploadedImages;
+  }
+  const result = await Creator.findByIdAndUpdate(id, payload, { new: true });
+  if (!result) throw new AppError(404, 'Creator not found');
+  return result;
+};
+
+const deleteCreator = async (id: string) => {
+  const result = await Creator.findByIdAndDelete(id);
+  if (!result) throw new AppError(404, 'Creator not found');
+  return result;
+};
+
+const updatedStatus = async (id: string, status: ICreator['status']) => {
+  const result = await Creator.findByIdAndUpdate(id, { status }, { new: true });
+
+  if (!result) throw new AppError(404, 'Agent not found');
+
+  return result;
+};
+
 export const creatorService = {
   requestCreator,
+  getAllCreators,
+  singleCreator,
+  updatedCreator,
+  deleteCreator,
+  updatedStatus,
 };
